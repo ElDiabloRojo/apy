8478eb9aa3251415078dacdf2c0eb738a4ddc265
0sum <holdens.uk@googlemail.com>
2020-01-08 13:05:44 +0000

    refactored to MONGODB_URI

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGODB_URI'] = os.environ.get('MONGODB_URI')
 app.config.from_object('api.config')
 #app.config.from_envvar('API_CONFIG')
 
